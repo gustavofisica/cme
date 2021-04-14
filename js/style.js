f70d78bab72ab12ca2edcfebe60324469829b0a0
Gustavo <gustavofisica@gmail.com>
2021-04-13 23:16:56 -0300

    Estilizando as primeiras páginas secundárias

--- js/style.js
+++ js/style.js
@@ -16,6 +16,24 @@ var bg = images[Math.floor(Math.random() * images.length)];
 
 backgroundImage.style.backgroundImage = "url(" + bg + ")";
 
+//Função do Menu Responsivo
+var menuTogle = document.querySelector('.menu-toggle');
+var bar = document.querySelector('.menu-toggle i');
+var menuUl = document.querySelector('.menu ul');
+
+menuTogle.addEventListener("click", (e) => {
+    
+    if (menuUl.classList == "") {
+        menuUl.classList.toggle("on");
+        bar.classList.remove("fa-bars");
+        bar.classList.add("fa-times");
+    } else {
+        menuUl.classList.toggle("on");
+        bar.classList.remove("fa-times");
+        bar.classList.add("fa-bars");
+    }
+});
+
 //Função do Slider
 var slideIndex = 1;
 showSlides(slideIndex);
@@ -47,21 +65,3 @@ function showSlides(n) {
     slides[slideIndex - 1].style.display = "block";
     dots[slideIndex - 1].className += " active";
 }
-
-//Função do Menu Responsivo
-var menuTogle = document.querySelector('.menu-toggle');
-var bar = document.querySelector('.menu-toggle i');
-var menuUl = document.querySelector('.menu ul');
-
-menuTogle.addEventListener("click", (e) => {
-    e.stopPropagation();
-    if (menuUl.classList == "") {
-        menuUl.classList.toggle("on");
-        bar.classList.remove("fa-bars");
-        bar.classList.add("fa-times");
-    } else {
-        menuUl.classList.toggle("on");
-        bar.classList.remove("fa-times");
-        bar.classList.add("fa-bars");
-    }
-});
