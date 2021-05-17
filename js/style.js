af1c627b7ca3c272f7100ff1d5af6bac53859580
Gustavo <gustavofisica@gmail.com>
2021-05-17 20:08:34 -0300

    Incorporando galeria e funções

--- js/style.js
+++ js/style.js
@@ -69,3 +69,73 @@ if (portifolio != null) {
         dots[slideIndex - 1].className += " active";
     }
 }
+
+//Função da galeria de imagens
+let modal = document.querySelector('.modal');
+let imagem = document.querySelectorAll('.galeria img');
+let imagemModal = document.querySelector('.imagem-modal');
+let textoModal = document.querySelector('.descricao-modal');
+let imagemModalGaleria = document.querySelector('.imagem-modal-portifolio');
+let textoAmostraModal = document.querySelector('.portifolio-amostra');
+let textoEquipamentoModal = document.querySelector('.portifolio-decricao-equipamento');
+let textoDescricaoModal = document.querySelector('.portifolio-decricao-texto');
+let pesquisadorDescricaoModal = document.querySelector('.descricao-pessoas-pesquisador');
+let departamentoDescricaoModal = document.querySelector('.descricao-pessoas-departamento');
+let tecnicoDescricaoModal = document.querySelector('.descricao-pessoas-tecnico');
+let cargoTecnicoDescricaoModal = document.querySelector('.descricao-pessoas-tecnico-cargo');
+let botaoFecharModal = document.querySelector('.fechar-modal')
+
+
+if (imagemModal != null) {
+    imagem.forEach(imagem => {
+        imagem.addEventListener('click', () => {
+            modal.classList.add('abrir');
+            let srcModal = imagem.getAttribute("data-original");
+            imagemModal.src = srcModal;
+            let altTexto = imagem.alt;
+            textoModal.textContent = altTexto;
+        });
+    });
+} else if (imagemModalGaleria != null) {
+    imagem.forEach(imagem => {
+        imagem.addEventListener('click', () => {
+            modal.classList.add('abrir');
+            let srcModal = imagem.getAttribute("data-original");
+            let amostraModal = imagem.getAttribute("data-amostra");
+            let equipamentoModal = imagem.getAttribute("data-equipamento");
+            let descricaoModal = imagem.getAttribute("data-descricao");
+            let pesquisadorModal = imagem.getAttribute("data-pesquisador");
+            let departamentoModal = imagem.getAttribute("data-departamento");
+            let tecnicoModal = imagem.getAttribute("data-tecnico");
+            let cargoModal = imagem.getAttribute("data-cargo");
+            imagemModalGaleria.src = srcModal;
+            textoAmostraModal.textContent = amostraModal;
+            textoEquipamentoModal.textContent = equipamentoModal;
+            textoDescricaoModal.textContent = descricaoModal;
+            pesquisadorDescricaoModal.textContent = pesquisadorModal;
+            departamentoDescricaoModal.textContent = departamentoModal;
+            tecnicoDescricaoModal.textContent = tecnicoModal;
+            cargoTecnicoDescricaoModal.textContent = cargoModal;
+        });
+    });
+}
+
+modal.addEventListener('click', fecharModal);
+if (botaoFecharModal != null) {
+    botaoFecharModal.addEventListener('click', removeAbrirModal);
+};
+document.addEventListener('keydown', (e) => {
+    if (e.keyCode == 27) {
+        removeAbrirModal();
+    }
+});
+
+function fecharModal(e) {
+    if (e.target.classList.contains('modal')) {
+        removeAbrirModal();
+    }
+}
+
+function removeAbrirModal() {
+    modal.classList.remove('abrir');
+}
